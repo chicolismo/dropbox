4128204cca15b62180dbf40bed7b5e1c91735759
lari <lari>
2017-05-04 15:00:02 -0300

    server variable names, thread loop

--- src/dropboxServer.c
+++ src/dropboxServer.c
@@ -6,17 +6,24 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 
-
-//CODIGO DE TUTORIAL DADO NA AULA 09
-
-#define PORT 4000
+#define MIN_AGR 1
+#define MAX_CONNECTIONS 5
+#define BUFFER_SIZE 256
 
 int main(int argc, char *argv[])
 {
 	int sockfd, newsockfd, n;
-	socklen_t clilen;
-	char buffer[256];
-	struct sockaddr_in serv_addr, cli_addr;
+	socklen_t client_len;
+	struct sockaddr_in serv_addr, client_addr;
+	char buffer[BUFFER_SIZE];
+	
+	if(argc < MIN_ARG)
+	{
+		printf("Not enough arguments passed.");
+		exit(1);
+	}
+	
+	int PORT = atoi(argv[1]);
 	
 	if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) == -1) 
         printf("ERROR opening socket");
@@ -29,26 +36,37 @@ int main(int argc, char *argv[])
 	if (bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) 
 		printf("ERROR on binding");
 	
-	listen(sockfd, 5);
-	
-	clilen = sizeof(struct sockaddr_in);
-	if ((newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen)) == -1) 
-		printf("ERROR on accept");
-	
-	bzero(buffer, 256);
-	
-	/* read from the socket */
-	n = read(newsockfd, buffer, 256);
-	if (n < 0) 
-		printf("ERROR reading from socket");
-	printf("Here is the message: %s\n", buffer);
-	
-	/* write in the socket */ 
-	n = write(newsockfd,"I got your message", 18);
-	if (n < 0) 
-		printf("ERROR writing to socket");
+	while(true)
+	{
+		listen(sockfd, MAX_CONNECTIONS);
+		
+		client_len = sizeof(struct sockaddr_in);
+		if ((newsockfd = accept(sockfd, (struct sockaddr *) &client_addr, &client_len)) == -1) 
+			printf("ERROR on accept");
+		
+		//http://stackoverflow.com/questions/3719462/sockets-and-threads-using-c
+		//o problema aqui é: o newsockfd nao vai ficar sendo sobreescrito sempre que criar uma nova thread?
+		//devemos fazer o accept dentro da thread talvez?
+		//se não for dentro da thread, onde a gente da close no socket?
+		pthread_t thread;
+		pthread_create(&thread, NULL, run_client, &newsockfd);
+		
+		bzero(buffer, BUFFER_SIZE);
+		
+		/* read from the socket */
+		n = read(newsockfd, buffer, BUFFER_SIZE);
+		if (n < 0) 
+			printf("ERROR reading from socket");
+		printf("Here is the message: %s\n", buffer);
+		
+		/* write in the socket */ 
+		n = write(newsockfd,"I got your message", 18);
+		if (n < 0) 
+			printf("ERROR writing to socket");
 
-	close(newsockfd);
-	close(sockfd);
+		close(newsockfd);
+		close(sockfd);
+	}
+	
 	return 0; 
 }
