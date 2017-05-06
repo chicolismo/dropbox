b8e4f170005ff753ff111e21f400f7334ea9743c
lari <lari>
2017-05-06 00:59:08 -0300

    server arrumado

--- src/dropboxServer.c
+++ src/dropboxServer.c
@@ -6,16 +6,40 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 
-#define MIN_AGR 1
+#define MIN_ARG 1
 #define MAX_CONNECTIONS 5
 #define BUFFER_SIZE 256
 
+
+void run_thread(void *socket_client)
+{
+	char buffer[BUFFER_SIZE];
+	int socket = *(int*)socket_client;
+	int message;
+	
+	bzero(buffer, BUFFER_SIZE);
+	
+	//read
+	message = read(socket, buffer, BUFFER_SIZE);
+	if (message < 0) 
+			printf("ERROR reading from socket");
+	printf("Here is the message: %s\n", buffer);
+	
+	// write
+	message = write(socket,"I got your message", 18);
+	if (message < 0) 
+		printf("ERROR writing to socket");
+
+	close(socket);
+}
+
+
 int main(int argc, char *argv[])
 {
-	int sockfd, newsockfd, n;
+	int socket_connection, socket_client;
 	socklen_t client_len;
 	struct sockaddr_in serv_addr, client_addr;
-	char buffer[BUFFER_SIZE];
+
 	
 	if(argc < MIN_ARG)
 	{
@@ -25,7 +49,7 @@ int main(int argc, char *argv[])
 	
 	int PORT = atoi(argv[1]);
 	
-	if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) == -1) 
+	if ((socket_connection = socket(AF_INET, SOCK_STREAM, 0)) == -1) 
         printf("ERROR opening socket");
 	
 	serv_addr.sin_family = AF_INET;
@@ -33,39 +57,21 @@ int main(int argc, char *argv[])
 	serv_addr.sin_addr.s_addr = INADDR_ANY;
 	bzero(&(serv_addr.sin_zero), 8);     
     
-	if (bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) 
+	if (bind(socket_connection, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) 
 		printf("ERROR on binding");
 	
-	while(true)
+	listen(socket_connection, MAX_CONNECTIONS);
+	client_len = sizeof(struct sockaddr_in);
+	
+	while( (socket_client = accept(socket_connection, (struct sockaddr *) &client_addr, &client_len)) )
 	{
-		listen(sockfd, MAX_CONNECTIONS);
+		pthread_t client_thread;
+		int *socket_thread = socket_client;
 		
-		client_len = sizeof(struct sockaddr_in);
-		if ((newsockfd = accept(sockfd, (struct sockaddr *) &client_addr, &client_len)) == -1) 
-			printf("ERROR on accept");
+		pthread_create(&client_thread, NULL, run_client, (void*)socket_thread);
 		
-		//http://stackoverflow.com/questions/3719462/sockets-and-threads-using-c
-		//o problema aqui é: o newsockfd nao vai ficar sendo sobreescrito sempre que criar uma nova thread?
-		//devemos fazer o accept dentro da thread talvez?
-		//se não for dentro da thread, onde a gente da close no socket?
-		pthread_t thread;
-		pthread_create(&thread, NULL, run_client, &newsockfd);
+		pthread_detach(client_thread);
 		
-		bzero(buffer, BUFFER_SIZE);
-		
-		/* read from the socket */
-		n = read(newsockfd, buffer, BUFFER_SIZE);
-		if (n < 0) 
-			printf("ERROR reading from socket");
-		printf("Here is the message: %s\n", buffer);
-		
-		/* write in the socket */ 
-		n = write(newsockfd,"I got your message", 18);
-		if (n < 0) 
-			printf("ERROR writing to socket");
-
-		close(newsockfd);
-		close(sockfd);
 	}
 	
 	return 0; 
