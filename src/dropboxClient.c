ce8ba8c1d60e789d8e2fc9a4d876590579b75cb9
lari <lari>
2017-05-06 00:58:50 -0300

    client arrumado

--- src/dropboxClient.c
+++ src/dropboxClient.c
@@ -9,27 +9,31 @@
 
 //CODIGO DE TUTORIAL DADO NA AULA 09
 
-#define PORT 4000
+#define MIN_ARG 3
+#define BUFFER_SIZE 256
 
 int main(int argc, char *argv[])
 {
-    int sockfd, n;
+    int socket, message;
     struct sockaddr_in serv_addr;
     struct hostent *server;
+    char buffer[BUFFER_SIZE];
 	
-    char buffer[256];
-    if (argc < 2) {
+    if (argc < MIN_ARG) 
+	{
 		fprintf(stderr,"usage %s hostname\n", argv[0]);
 		exit(0);
     }
 	
-	server = gethostbyname(argv[1]);
-	if (server == NULL) {
+	int PORT = atoi(argv[3]);
+	
+	if ( (server = gethostbyname(argv[1])) == NULL ) 
+	{
         fprintf(stderr,"ERROR, no such host\n");
         exit(0);
     }
     
-    if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) == -1) 
+    if ((socket = socket(AF_INET, SOCK_STREAM, 0)) == -1) 
         printf("ERROR opening socket\n");
     
 	serv_addr.sin_family = AF_INET;     
@@ -38,27 +42,31 @@ int main(int argc, char *argv[])
 	bzero(&(serv_addr.sin_zero), 8);     
 	
     
-	if (connect(sockfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0) 
+	if (connect(socket,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0) 
         printf("ERROR connecting\n");
 
-    printf("Enter the message: ");
-    bzero(buffer, 256);
-    fgets(buffer, 256, stdin);
-    
-	/* write in the socket */
-	n = write(sockfd, buffer, strlen(buffer));
-    if (n < 0) 
-		printf("ERROR writing to socket\n");
 
-    bzero(buffer,256);
-	
-	/* read from the socket */
-    n = read(sockfd, buffer, 256);
-    if (n < 0) 
-		printf("ERROR reading from socket\n");
-
-    printf("%s\n",buffer);
     
-	close(sockfd);
+	while(true) //mudar tudo aqui pro trabalho
+	{
+		printf("Enter the message: ");
+		bzero(buffer, BUFFER_SIZE);
+		fgets(buffer, BUFFER_SIZE, stdin);
+		
+		message = write(socket, buffer, strlen(buffer));
+		if (message < 0) 
+			printf("ERROR writing to socket\n");
+
+		bzero(buffer,BUFFER_SIZE);
+		
+		/* read from the socket */
+		message = read(socket, buffer, BUFFER_SIZE);
+		if (message < 0) 
+			printf("ERROR reading from socket\n");
+
+		printf("%s\n",buffer);
+		
+		close(socket);
+	}
     return 0;
 }
