9d0dd45df4b24f76272a12d9821b942782d0d394
mafoltz <marcelo.andrighetto@hotmail.com>
2017-06-08 08:56:42 -0300

    Server receive file complete

--- src/dropboxServer.c
+++ src/dropboxServer.c
@@ -7,31 +7,47 @@ void receive_file(char *file, int client_socket)
 	int fd, verification_fd;
 	struct stat file_stat;
 	ssize_t len;
-	int remain_data;
-	char file_size[256];
-	int offset;
-	int sent_bytes = 0;
+	char buffer[256];
+	int file_size;
 
 	// Open file
-	verification_fd = open(file, O_RDONLY);
-	if (verification_fd == -1)
-    {
-		// File doesn't exist yet, it must be created
-		fd = open(file, O_CREAT, S_IRUSR); // | S_IWUSR | S_IXUSR);
-    }
-	else
-	{
-		// File already exist
-		fd = open(file, O_APPEND);
-	}
+    fd = open(file, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (fd == -1)
     {
 		printf("Error opening file");
 		exit(EXIT_FAILURE);
     }
 
+	// Receive file size
+	len = recv(client_socket, buffer, sizeof(buffer), 0);
+	if (len < 0)
+	{
+		printf("Error on receiving file size");
+		exit(EXIT_FAILURE);
+	}
+	file_size = atoi(buffer);
+	printf("Server receive %d bytes for the file size\n", len);
+
 	// Receive file data
-	// TODO
+	while(lseek(fd, 0, SEEK_END) < file_size)
+	{
+		ssize_t read_len = recv(client_socket, buffer, sizeof(buffer), 0);
+		if (read_len < 0)
+		{
+			printf("Error on receiving file data");
+			exit(EXIT_FAILURE);
+		}
+
+		// Write file data
+		len = write(fd, buffer, read_len);
+		if (len < 0)
+		{
+			printf("Error on writing file");
+			exit(EXIT_FAILURE);
+		}
+	}
+
+	close(fd);
 }
 
 void send_file(char *file, int client_socket)
@@ -77,6 +93,8 @@ void send_file(char *file, int client_socket)
 		remain_data -= sent_bytes;
 		printf("Server sent %d bytes from file's data, offset is now : %d and remaining data = %d\n", sent_bytes, offset, remain_data);
 	}
+
+	close(fd);
 }
 
 void run_thread(void *socket_client)
