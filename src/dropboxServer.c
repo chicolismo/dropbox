2ff99e929f706f7677513394763e44826cc45554
lahagemann <lahagemann@inf.ufrgs.br>
2017-06-25 19:25:04 -0300

    resolvendo bug do commit atrasado

--- src/dropboxServer.c
+++ src/dropboxServer.c
@@ -478,14 +478,9 @@ void sync_server(int socketfd)
 			else				// arquivo não existe no servidor
 			{
 				printf("file doesn't exist. mirror cc %d, server cc %d\n", client_mirror.current_commit, connected_clients[cliindex].current_commit);
-				int server_commit = connected_clients[cliindex].current_commit;
 
-				// device 2 executando
-				if(socketfd != connected_clients[cliindex].devices[0])	
-					server_commit -= 1;
-
-				// verifica se o arquivo no cliente tem um commit_modified > state do servidor
-				if(client_mirror.current_commit == server_commit)
+				// verifica se o arquivo no cliente tem um state <= state do servidor
+				if((client_mirror.current_commit == connected_clients[cliindex].current_commit) || (client_mirror.current_commit == (connected_clients[cliindex].current_commit - 1)))
 				{
 					//isso quer dizer que é um arquivo novo colocado no servidor em outro pc.
 					// pede para o cliente mandar o arquivo
