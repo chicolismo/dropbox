39ebe779d481d0040457f1a19724dd0b6cf322c3
lari <lrzgoncalves@gmail.com>
2017-06-25 21:26:58 -0300

    nmhfgdsjkhgh

--- src/dropboxClient.c
+++ src/dropboxClient.c
@@ -229,7 +229,7 @@ void* sync_client(void *socket_sync)
 					printf("i don't exist\n");
 					//int self_commit = self.current_commit;
 		
-					if(self.current_commit == (server_mirror.current_commit - 1))
+					if(self.current_commit > server_mirror.fileinfo[i].commit_modified)
 					{
 						// o arquivo é velho e deve ser deletado do servidor adequadamente.
 						bzero(buffer, BUFFER_SIZE);
